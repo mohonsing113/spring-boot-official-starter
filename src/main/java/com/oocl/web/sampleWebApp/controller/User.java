8f0b66b4979ee9d34f59b8d6872a90ed692cfab4
Mo Hon Sing <mohonsing13@gmail.com>
2018-11-29 00:21:02 +0800

    add test case

---
+++ src/main/java/com/oocl/web/sampleWebApp/controller/User.java
@@ -0,0 +1,20 @@
+package com.oocl.web.sampleWebApp.controller;
+
+public class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
